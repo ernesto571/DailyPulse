f3853a22857297d589da1a6b1fe8a81728e16073
ernesto571 <emmanueladesemoye@gmail.com>
2025-11-04 16:34:21 +0100

    third commit

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "https://cruz-dailypulse.netlify.app" || "http://localhost:5173",
+    origin: ["https://cruz-dailypulse.netlify.app" , "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
